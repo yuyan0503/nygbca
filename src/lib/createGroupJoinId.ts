295149fc02408a5c18aee5356d2355823988da2b
g6intdev <letslook460@gmail.com>
2024-11-17 18:47:41 +0800

    converted createGroupJoinId to ts

--- src/lib/createGroupJoinId.ts
+++ src/lib/createGroupJoinId.ts
@@ -1,6 +1,6 @@
 import prisma from "./prisma";
 
-export default async function createGroupJoinId(joiningGroupId, isMaster) {
+export default async function createGroupJoinId(joiningGroupId:number, isMaster:boolean) {
   const groupJoinId = await prisma.groupJoinId.create({
     data: {
       isMaster,
